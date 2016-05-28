ec2cefab07a6ae2d401913327db84d5575a3c9af
Shane Goodson <shane@webzai.com>
2016-05-28 22:59:05 +0300

    Started work on the server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,45 @@
-/**
- * Created by USER on 28/05/2016.
- */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var _trackData = {
+    trackLength: 400,
+    maxPace: 40,
+    minPace: 10,
+    paceSize: 5
+};
+
+var _horseNamePool = [
+    "Agile", "Ferdinand", "Clyde", "Meridian", "Cannonade", "Apollo", "Fonso", "Chant", "Riley", "Cavalcade", "Swale", "Barbaro", "Rosebud", "Montrose", "Silver Charm", "Grindstone"
+];
+
+var _horseAvatarPool = [];
+
+var _bettingData = {
+    bettingPool: 0,
+    bets: []
+};
+
+//TODO: Current config works with one socket, figure out how to manage and use multiple sockets.
+
+io.on('connection', function(socket){
+    //TODO:See how to get client data and assign id
+    console.log('client app connected');
+
+    socket.emit('getRaceData', null /*use race manager to process relevant data for client on initial connect*/);
+
+    socket.on('lockBet', function (betData) {
+       //use id to save bet amount and horse per user and increment betting pool
+    });
+
+    socket.on('pullOut', function () {
+        //deregister client from bettingData
+    });
+});
+
+
+http.listen(8080, function(){
+    console.log('listening on *:8080');
+});
+
+
